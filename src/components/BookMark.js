de42dc672f70fe6a65a352ac42971b153d585d06
Abdul Waqar <abdulwaqar844@gmaill.com>
2021-01-19 22:32:19 +0500

    url section updated

--- src/components/BookMark.js
+++ src/components/BookMark.js
@@ -31,7 +31,7 @@ export const BookMark = (prop) => {
         <div className={style.content}>
             <div className={style.bookmark} key={prop.id}>
                 <h3>{prop.title}</h3>
-                <h3>{prop.url}</h3>
+                <h3><a href={`https://${prop.url}`}>{prop.url}</a></h3>
                 <Button variant="contained" color="secondary" type="submit"     onClick={() => handleDelete(prop.id)}>Delete</Button> 
             </div>
         </div>
